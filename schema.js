35597028cfc7b67916a183e08f709550cbd9803c
Atuma David <atumadavid35@gmail.com>
2024-07-23 10:08:04 +0100

    db,schema, resolvers

--- schema.js
+++ schema.js
@@ -20,7 +20,12 @@ export const typeDefs = `#graphql
 # compulsory --Query "entry points"
     type Query{
         reviews: [Review]
+        review(id: ID!): Review
+
         games: [Game]
+        game(id: ID!): Game
+        
         authors: [Author]
+        author(id: ID!): Author
     }
 `;
